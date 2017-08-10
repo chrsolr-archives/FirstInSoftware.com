75ea4dc57adf64d97213e132962dac6ecc245e02
iamrelos <twenty40@gmail.com>
2017-08-09 23:39:52 -0500

    updated

--- src/js/home.js
+++ src/js/home.js
@@ -4,4 +4,16 @@ $(function () {
     $('.navbar').bootstrapAutoHideNavbar({
         shadow: false
     });
+
+    $(window).scroll(function () {
+        $('.hide-element').each(function () {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var position_to_animate_element = parseInt(($element.offset().top + element_height) - (element_height / 2), 10);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > $element.offset().top + 60)
+                $(this).addClass('animated fadeIn');
+        });
+    });
 });
